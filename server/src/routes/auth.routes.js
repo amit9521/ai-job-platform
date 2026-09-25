7c67aeb7d527fd7771e6fec1c7b58452b83cc5ea
amit9521 <amit160199@gmail.com>
2026-09-25 12:11:47 +0530

    feat: add user login API

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,11 +1,13 @@
-
-
 import express from "express";
-import { registerUser } from "../controllers/auth.controller.js";
+import { registerUser, loginUser } from "../controllers/auth.controller.js";
+
 import { validateRegister } from "../middlewares/validateRegister.js";
+import { validateLogin } from "../middlewares/validateLogin.js";
 
 const router = express.Router();
 
 router.post("/register", validateRegister, registerUser);
+
+router.post("/login", validateLogin, loginUser);
 
 export default router;
